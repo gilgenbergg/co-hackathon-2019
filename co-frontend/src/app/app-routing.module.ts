dfb9418415cef20ed22df83f09f2c7b1fec54bf0
Matthias Krawanja <krawanja@rwrbrille.at>
2019-10-11 16:04:57 +0200

    implement basic routing

--- co-frontend/src/app/app-routing.module.ts
+++ co-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './guards';
 
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
---
+++ co-frontend/src/app/services/authentication.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private currentUserSubject: BehaviorSubject<any>;
+    public currentUser: Observable<any>;
+    private urlbase: string = 'http://localhost:8080/co-backend/';
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue() {
+        return this.currentUserSubject.value;
+    }
+
+    login(username, password) {
+        return this.http.post<any>(this.urlbase + `users/authenticate`, { username, password })
+            .pipe(map(user => {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('currentUser', JSON.stringify(user));
+                this.currentUserSubject.next(user);
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage and set current user to null
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+}
