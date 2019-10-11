778ddd2ada164be8317aa54d85d7ef6846c82178
Gernot Ohner <gernot.ohner@student.tugraz.at>
2019-10-11 16:47:39 +0200

    Complete User Story: A secret between us

--- co-backend/src/main/java/org/campusonline/hackathon/COHackathonRESTService.java
+++ co-backend/src/main/java/org/campusonline/hackathon/COHackathonRESTService.java
@@ -3,9 +3,12 @@ package org.campusonline.hackathon;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Path("/")
@@ -14,15 +17,41 @@ public class COHackathonRESTService {
     @GET
     @Path("time")
     @Produces("application/json")
-    public Response getService() {
+    public Response getTimeService() {
 
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
         Date date = new Date();
         final JsonObject jsonObject = Json.createObjectBuilder()
-                .add("text", date.toString()).build();
+                .add("text", formatter.format(date)).build();
 
         return Response.status(Response.Status.OK)
                 .entity(jsonObject)
                 .build();
+
+    }
+
+    @POST
+    @Path("secret")
+    @Produces("application/json")
+    public Response getSecretService(String message) {
+
+        String secretMessage = "{\n\t\"secret\" : \"aligator3\"\n}";
+
+        if (message.equals(secretMessage)) {
+            final JsonObject jsonObject = Json.createObjectBuilder()
+                    .add("text", "CORRECT MESSAGE").build();
+
+            return Response.status(Response.Status.OK)
+                    .entity(jsonObject)
+                    .build();
+        } else {
+            final JsonObject jsonObject = Json.createObjectBuilder()
+                    .add("text", "WRONG MESSAGE").build();
+            return Response.status(418, "I'm a teapot.")
+                    .entity(jsonObject)
+                    .build();
+        }
+
 
     }
 }
